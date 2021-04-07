56aa83bcd84e69c2df213f9c23b3e547790a224f
Bryan Ledda <bledda@c2r4p1.42nice.fr>
2021-04-07 10:58:56 +0200

    update

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 13:18:10 by bledda            #+#    #+#             */
-/*   Updated: 2021/03/23 18:32:07 by bledda           ###   ########.fr       */
+/*   Updated: 2021/04/05 22:52:52 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *s, int c)
 	size_t	i;
 
 	i = 0;
-	while (i < ft_strlen(s) + 1)
+	while (s[i] != 0)
 	{
 		if (s[i] == (char)c)
 			return ((char *)s + i);
